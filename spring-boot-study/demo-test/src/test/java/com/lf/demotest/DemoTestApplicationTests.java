cb7b131f50dda9f2b6e757311e79373429c4fc5d
linfan003 <linfan0022@gmail.com>
2020-12-14 15:54:24 +0800

    spring boot-test-demo

---
+++ spring-boot-study/demo-test/src/test/java/com/lf/demotest/DemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lf.demotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
